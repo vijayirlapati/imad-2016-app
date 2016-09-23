7cf6fc740aa596c87db0be698ab05179a7ff6f73
vijayirlapati <irlapativijay4@gmail.com>
2016-09-24 03:54:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName',function(req,res){
     //article name will be article one
-    var articleName=rec.params.articleName;
+    var articleName=req.params.articleName;
 res.send(createTemplate(articles[articleName]));
     
 });
