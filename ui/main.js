ce414763f92627a9c6bff54e57a52cfc6b502933
vijayirlapati <irlapativijay4@gmail.com>
2016-09-27 22:55:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,11 @@ var element=document.getElementById('main-text');
 element.innerHTML='New Element';
 //move the image
 var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
 img.onclick=function(){
-    img.style.marginLeft="100px";
+    var interval=setInterval(moveRight,100);
 };
