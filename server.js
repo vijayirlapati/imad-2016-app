848c92f789471c8327dc5ae1de64e036dabb7dd5
vijayirlapati <irlapativijay4@gmail.com>
2016-09-27 23:39:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,6 +86,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter=0;
+app.get('/counter',function(req,res){
+   counter=counter+1;
+   res.send(counter.toString());
+});
+
+
+
 app.get('/:articleName',function(req,res){
     //article name will be article one
     var articleName=req.params.articleName;
