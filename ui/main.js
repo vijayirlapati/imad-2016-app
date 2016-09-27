aadce7b3b742285446104f94650466018de30376
vijayirlapati <irlapativijay4@gmail.com>
2016-09-27 22:38:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,10 @@ console.log('Loaded!');
 //change the text of main-text div
 var element=document.getElementById('main-text');
 element.innerHTML='New Element';
+//move the image
+var image=document.getElementById('img');
+img.onclick=function(){
+    img.style.margimLeft="100px";
+    
+    
+};
