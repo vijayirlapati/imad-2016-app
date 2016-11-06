f252e7e47816767c3d90dc1025143895583d71fd
vijayirlapati <irlapativijay4@gmail.com>
2016-11-06 19:27:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,7 +31,9 @@ submit.onclick=function(){
     //capture list of names and render it as a list
     var names=["name1","name2","name3","name4"];
     var list="";
-    for(var i+0;i<names.length;i++){
+    for(var i+0;i<names.length;i++);
+    
+    {
         list+="<li>"+names[i]+"</li>";
     }
     var ul=document.getElementById("namelist");
